992d807b831975f6b89e3c9d8b996596bdd49660
Rahul Anand <104300673+Rahul0550@users.noreply.github.com>
2023-05-21 10:16:49 +0530

    Img src added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/App.css';
+import programing from "../assets/programing"
 
 const GIF = () => {
   return (
@@ -8,12 +9,13 @@ const GIF = () => {
 }
 
 const App = () => {
+  const imageUrl = 'https://picsum.photos/id/400/200';
 
   return (
     <div id="main">
       <h1>Add images in 2-different ways.</h1>
-      <GIF />
-      <img className='url-image' />
+      <GIF src={programing} alt="programing-gif"/>
+      <img className='url-image' src={imageUrl} alt="A random image" />
     </div>
   )
 }
